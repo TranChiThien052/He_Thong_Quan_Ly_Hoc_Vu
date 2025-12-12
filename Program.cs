c91d47812cc8a82a1c3113651bb61f9a071bf307
ThienTran <thientranchi052@gmail.com>
2025-12-12 20:59:13 +0700

    Cấu hình database

--- Program.cs
+++ Program.cs
@@ -1,11 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<QuanLyHocVu.Models.QuanLyHocVuContext>();
+builder.Services.AddDbContext<QuanLyHocVu.Models.QuanLyHocVuContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 // Đăng ký Service vào Container
 builder.Services.AddScoped<QuanLyHocVu.Services.IMonHocService, QuanLyHocVu.Services.MonHocService>();
+builder.Services.AddScoped<QuanLyHocVu.Services.ISinhVienService, QuanLyHocVu.Services.SinhVienService>();
+builder.Services.AddScoped<QuanLyHocVu.Services.IGiangVienService, QuanLyHocVu.Services.GiangVienService>();
+builder.Services.AddScoped<QuanLyHocVu.Services.ILopHocPhanService, QuanLyHocVu.Services.LopHocPhanService>();
+builder.Services.AddScoped<QuanLyHocVu.Services.IDangKyHocPhanService, QuanLyHocVu.Services.DangKyHocPhanService>();
+builder.Services.AddScoped<QuanLyHocVu.Services.IKhoaService, QuanLyHocVu.Services.KhoaService>();
+builder.Services.AddScoped<QuanLyHocVu.Services.INganhService, QuanLyHocVu.Services.NganhService>();
+builder.Services.AddScoped<QuanLyHocVu.Services.IHocKyService, QuanLyHocVu.Services.HocKyService>();
+builder.Services.AddScoped<QuanLyHocVu.Services.IPhongHocService, QuanLyHocVu.Services.PhongHocService>();
+builder.Services.AddScoped<QuanLyHocVu.Services.IChuongTrinhDaoTaoService, QuanLyHocVu.Services.ChuongTrinhDaoTaoService>();
+builder.Services.AddScoped<QuanLyHocVu.Services.IHocPhiService, QuanLyHocVu.Services.HocPhiService>();
+
+// Thêm Authentication Service
+builder.Services.AddAuthentication(Microsoft.AspNetCore.Authentication.Cookies.CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login";
+        options.AccessDeniedPath = "/Account/AccessDenied";
+    });
 
 var app = builder.Build();
 
@@ -25,6 +44,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication(); // BẮT BUỘC: Phải đứng trước Authorization
 app.UseAuthorization();
 
 // Định tuyến cho Areas (QUAN TRỌNG: Phải đặt trước route mặc định)
