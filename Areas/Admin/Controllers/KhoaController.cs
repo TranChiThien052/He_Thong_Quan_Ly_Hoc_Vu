e4825a7b34433db05bb690f9b9ec24407cc540b1
ThienTran <thientranchi052@gmail.com>
2025-12-13 21:53:01 +0700

    Tạo controller xử lý thêm khoa

---
+++ Areas/Admin/Controllers/KhoaController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QuanLyHocVu.Models;
+using QuanLyHocVu.Services;
+
+namespace QuanLyHocVu.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class KhoaController : Controller
+    {
+        private readonly QuanLyHocVuContext _context;
+        private readonly IKhoaService _service;
+        public KhoaController(QuanLyHocVuContext context, IKhoaService service)
+        {
+            _context = context;
+            _service = service;
+        }
+        
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create([Bind("MaNganh,TenNganh")] QuanLyHocVu.Models.Khoa khoa)
+        {
+            _service.Add(khoa);
+            return RedirectToAction("Index", "KhoaNganh");
+        }
+    }
+}
+
