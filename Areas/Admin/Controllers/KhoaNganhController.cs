a544af58bb8f4adf5ab5adde077582cd3ed0670f
ThienTran <thientranchi052@gmail.com>
2025-12-13 21:27:16 +0700

    Thêm controller KhoaNganh

---
+++ Areas/Admin/Controllers/KhoaNganhController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QuanLyHocVu.Models;
+using QuanLyHocVu.Services;
+
+namespace QuanLyHocVu.Areas.Admin.Controllers;
+
+public class KhoaNganhController : Controller
+{
+    private readonly QuanLyHocVuContext _context;
+    private readonly IKhoaService _khoaService;
+    private readonly INganhService _nganhService;
+
+    public KhoaNganhController(QuanLyHocVuContext context, IKhoaService khoaService, INganhService nganhService)
+    {
+        _context = context;
+        _khoaService = khoaService;
+        _nganhService = nganhService;
+    }
+
+    public IActionResult Index()
+    {
+        var khoas = _khoaService.GetAll();
+        var nganhs = _nganhService.GetAll();
+        ViewBag.Khoas = khoas;
+        ViewBag.Nganhs = nganhs;
+        return View();
+    }
+}
