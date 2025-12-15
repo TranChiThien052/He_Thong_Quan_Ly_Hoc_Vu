d7b135da3b2a0bb6302e8ab62a815aef87184095
Le Duong Anh Tuan <ldat161203@gmail.com>
2025-12-15 23:20:31 +0700

    hoan thanh them, hien thi, loc phong hoc. Con phan sua

---
+++ Areas/Admin/Controllers/PhongHocController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using QuanLyHocVu.Models;
+using QuanLyHocVu.Services;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+
+namespace QuanLyHocVu.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class PhongHocController : Controller
+    {
+        private readonly IPhongHocService _phongHocService;
+
+        public PhongHocController(IPhongHocService phongHocService)
+        {
+            _phongHocService = phongHocService;
+        }
+
+        public IActionResult Index(string loaiPhong, string tang)
+        {
+            var phongHoc = _phongHocService.GetAll();
+            if(!string.IsNullOrEmpty(loaiPhong)){
+                phongHoc = phongHoc.Where(p => p.LoaiPhong == loaiPhong).ToList();
+            }
+            if(!string.IsNullOrEmpty(tang)){
+                phongHoc = phongHoc.Where(p => p.Tang.ToString() == tang).ToList();
+            }
+            var loaiPhongList = _phongHocService.GetAll().Select(p => p.LoaiPhong).Distinct().ToList();
+            var tangList = _phongHocService.GetAll().Select(p => p.Tang).Distinct().ToList();
+            ViewBag.LoaiPhong = new SelectList(loaiPhongList);
+            ViewBag.Tang=new SelectList(tangList);
+            
+            return View(phongHoc);
+        }
+
+        public IActionResult Create(){
+            return View();
+        }   
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(PhongHoc phongHoc){
+            _phongHocService.Add(phongHoc);
+            return RedirectToAction("Index");
+        }
+    }
+}
