1a312bb9f3b978c625f8c03548b031d37b0e0c56
ThienTran <thientranchi052@gmail.com>
2025-12-29 17:37:46 +0700

    sửa giao diện cập nhật điểm

--- Areas/GiangVien/Controllers/LopHocPhanController.cs
+++ Areas/GiangVien/Controllers/LopHocPhanController.cs
@@ -1,11 +1,7 @@
 using System.Security.Claims;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using QuanLyHocVu.Services;
-using SQLitePCL;
 using QuanLyHocVu.Models;
 namespace QuanLyHocVu.Areas.GiangVien.Controllers
 
